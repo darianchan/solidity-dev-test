3a0f8d907fa7ca257f3ddf29cd1e233237c6e482
Darian <darian.s.chan@gmail.com>
2022-02-06 20:50:55 -0800

    added withdraw method

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -4,10 +4,17 @@ import "./MoonCoin.sol";
 
 // Exchange rate is 1 eth = 10 Moon Coins
 contract Exchange {
+  address public owner;
   MoonCoin public moonCoin;
 
   constructor() {
+    owner = msg.sender;
     moonCoin = new MoonCoin(address(this));
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
   }
 
   function swapETHForMoon(uint _moonCoinAmount) public payable {
@@ -15,6 +22,12 @@ contract Exchange {
     require(msg.value >= eth);
 
     (bool success) = moonCoin.transfer(msg.sender, _moonCoinAmount * 10 ** 18);
+    require(success);
+  }
+
+  // sends entire contract's balance to the owner
+  function withdraw() public onlyOwner {
+    (bool success, ) = owner.call{value: address(this).balance}("");
     require(success);
   }
   
